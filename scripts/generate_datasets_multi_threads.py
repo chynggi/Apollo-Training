44274661cd3b5c9ad4a4853f34a64cf009539add
Sucial <75163782+SUC-DriverOld@users.noreply.github.com>
2025-02-26 18:33:24 +0800

    datasets multi threads generator

--- scripts/generate_datasets_multi_threads.py
+++ scripts/generate_datasets_multi_threads.py
@@ -5,9 +5,10 @@ import numpy as np
 import soundfile as sf
 import traceback
 import subprocess
-from tqdm import tqdm
+from multiprocessing import cpu_count
+from concurrent.futures import ThreadPoolExecutor
 
-FFMPEG = "ffmpeg" # specify the path to ffmpeg
+FFMPEG = "ffmpeg"  # specify the path to ffmpeg
 
 def check_isvalid(args):
     try:
@@ -45,78 +46,33 @@ def check_isvalid(args):
 
 def to_mp3(args, input_file, output_path, sr=44100):
     bitrate = random.choice(args.bitrates)
-    if args.verbose:
-        print(f"Use bitrate {bitrate}")
-
     param = ""
     if args.enable_quality and random.random() < args.quality_possibility:
         quality = random.randint(args.quality_min, args.quality_max)
         param += f" -q:a {quality}"
-        if args.verbose:
-            print(f"Use Quality: {quality}")
-
     if args.enable_lowpass and random.random() < args.lowpass_possibility:
         value = random.randint(args.lowpass_min_freq, args.lowpass_max_freq)
         param += f' -af "lowpass=f={value}"'
-        if args.verbose:
-            print(f"Use Lowpass: {value}")
-
     to_mp3 = f"{FFMPEG} -i \"{input_file}\" -ar {sr} -ac 2 -b:a {bitrate} {param} -vn \"{output_path}\" -y"
-    if args.verbose:
-        print(f"Command: {to_mp3}")
     os.system(f"{to_mp3} > {os.devnull} 2>&1")
     return output_path
 
 
-def generate_dataset(args):
-    print(f"Total files find: {len(os.listdir(args.input_folder))}")
-    if args.verbose:
-        all_files = os.listdir(args.input_folder)
-    else:
-        all_files = tqdm(os.listdir(args.input_folder))
+def process_audio(args, audio_name, all_files):
+    print(f"[{all_files.index(audio_name) + 1}/{len(all_files)}]Processing {audio_name}")
+    base_name = os.path.splitext(audio_name)[0]
 
-    for audio_name in all_files:
-        if args.verbose:
-            total = len(all_files)
-            now = all_files.index(audio_name) + 1
-            print(f"\n[{now}/{total}]Processing {audio_name}")
-        else:
-            all_files.set_postfix_str(audio_name)
-        base_name = os.path.splitext(audio_name)[0]
+    try:
+        waveform, _ = librosa.load(os.path.join(args.input_folder, audio_name), sr=44100, mono=False)
+        if len(waveform.shape) == 1:
+            waveform = np.stack([waveform, waveform], axis=0)
+            print(f"Warning: {audio_name} is mono. Converting to stereo.")
+        if len(waveform.shape) != 1 and waveform.shape[0] > 2:
+            print(f"Warning: {audio_name} has more than 2 channels. Skipping.")
+            return
 
-        try:
-            waveform, _ = librosa.load(os.path.join(args.input_folder, audio_name), sr=44100, mono=False)
-            if len(waveform.shape) == 1:
-                waveform = np.stack([waveform, waveform], axis=0)
-                print(f"Warning: {audio_name} is mono. Converting to stereo.")
-            if len(waveform.shape) != 1 and waveform.shape[0] > 2:
-                print(f"Warning: {audio_name} has more than 2 channels. Skipping.")
-                continue
-
-            if args.generate_train:
-                if args.dataset_type == 1:
-                    store_dir = os.path.join(args.output_folder, base_name)
-                    os.makedirs(store_dir, exist_ok=True)
-                    to_mp3(
-                        args=args,
-                        input_file=os.path.join(args.input_folder, audio_name),
-                        output_path=os.path.join(store_dir, "codec.mp3")
-                    )
-                    sf.write(os.path.join(store_dir, "original.wav"), waveform.T, 44100)
-                elif args.dataset_type == 2:
-                    os.makedirs(os.path.join(args.output_folder, "original"), exist_ok=True)
-                    os.makedirs(os.path.join(args.output_folder, "codec"), exist_ok=True)
-                    to_mp3(
-                        args=args,
-                        input_file=os.path.join(args.input_folder, audio_name),
-                        output_path=os.path.join(args.output_folder, "codec", f"{base_name}.mp3")
-                    )
-                    sf.write(os.path.join(args.output_folder, "original", f"{base_name}.wav"), waveform.T, 44100)
-                else:
-                    print("ERROR: Invalid dataset type.")
-                    os._exit(1)
-
-            if args.generate_valid:
+        if args.generate_train:
+            if args.dataset_type == 1:
                 store_dir = os.path.join(args.output_folder, base_name)
                 os.makedirs(store_dir, exist_ok=True)
                 to_mp3(
@@ -125,11 +81,47 @@ def generate_dataset(args):
                     output_path=os.path.join(store_dir, "codec.mp3")
                 )
                 sf.write(os.path.join(store_dir, "original.wav"), waveform.T, 44100)
+            elif args.dataset_type == 2:
+                os.makedirs(os.path.join(args.output_folder, "original"), exist_ok=True)
+                os.makedirs(os.path.join(args.output_folder, "codec"), exist_ok=True)
+                to_mp3(
+                    args=args,
+                    input_file=os.path.join(args.input_folder, audio_name),
+                    output_path=os.path.join(args.output_folder, "codec", f"{base_name}.mp3")
+                )
+                sf.write(os.path.join(args.output_folder, "original", f"{base_name}.wav"), waveform.T, 44100)
+            else:
+                print("ERROR: Invalid dataset type.")
+                os._exit(1)
 
-        except Exception as e:
-            print(f"Cound not process {audio_name}. Error: {str(e)}")
-            traceback.print_exc()
-            continue
+        if args.generate_valid:
+            store_dir = os.path.join(args.output_folder, base_name)
+            os.makedirs(store_dir, exist_ok=True)
+            to_mp3(
+                args=args,
+                input_file=os.path.join(args.input_folder, audio_name),
+                output_path=os.path.join(store_dir, "codec.mp3")
+            )
+            sf.write(os.path.join(store_dir, "original.wav"), waveform.T, 44100)
+
+    except Exception as e:
+        print(f"Could not process {audio_name}. Error: {str(e)}")
+        traceback.print_exc()
+
+
+def generate_dataset(args):
+    print(f"Total files found: {len(os.listdir(args.input_folder))}")
+    all_files = os.listdir(args.input_folder)
+
+    # Using ThreadPoolExecutor for parallel processing
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+        futures = [
+            executor.submit(process_audio, args, audio_name, all_files)
+            for audio_name in all_files
+        ]
+        # Wait for all threads to finish
+        for future in futures:
+            future.result()
 
 
 if __name__ == "__main__":
@@ -140,7 +132,7 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--dataset_type", type=int, choices=[1, 2], default=1, help="Type of dataset to generate. See README for more details.")
     parser.add_argument("-gt", "--generate_train", action="store_true", help="Generate the training dataset.")
     parser.add_argument("-gv", "--generate_valid", action="store_true", help="Generate the validation dataset.")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode.")
+    parser.add_argument("-th", "--threads", type=int, default=cpu_count(), help="Number of threads to use for processing.")
     parser.add_argument("--bitrates", nargs="+", default=["64k", "96k", "128k", "192k", "256k", "320k"], help="List of bitrates to use for the mp3 conversion.")
     parser.add_argument("--enable_quality", action="store_true", help="Enable quality settings for the mp3 conversion.")
     parser.add_argument("--quality_possibility", type=float, default=1.0, help="Possibility of using quality settings for the mp3 conversion.")
